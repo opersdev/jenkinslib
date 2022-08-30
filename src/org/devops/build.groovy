ebd2c2f225e3bacff35f6b6a24dcadfbf09c1291
xiaonuo <38125526+opersdev@users.noreply.github.com>
2022-08-30 20:11:25 +0800

    Create build.groovy

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,15 @@
+package org.devops
+
+//构建类型
+
+def Build(buildType,buildShell){
+  def buildTools = ["mvn":"M2","npm":"NPM"]
+  println("当前选择的构建类型为 ${buildType}")
+  buildHome = tool buildTools[buildType]
+  
+  sh "${buildHome}/bin${buildType} $buildShell}"
+  
+}
+  
+ 
+  
